0350a835ddcf579cf2d62410ed0b374f67690cde
Maksim <maxgraph23@gmail.com>
2023-04-23 04:58:11 +0300

    test form 3

--- assets/mail.php
+++ assets/mail.php
@@ -33,15 +33,15 @@ try {
 
   // Настройки вашей почты
   $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-  $mail->Username   = ''; // Логин на почте
-  $mail->Password   = ''; // Пароль на почте
+  $mail->Username   = 'maxgraph23@gmail.com'; // Логин на почте
+  $mail->Password   = 'fsomktmprmtktavv'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
 
-  $mail->setFrom('', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
+  $mail->setFrom('maxgraph23@gmail.com', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
 
   // Получатель письма
-  $mail->addAddress('');
+  $mail->addAddress('maxgraph23@gmail.com');
 
   // Прикрипление файлов к письму
   if (!empty($file['name'][0])) {
